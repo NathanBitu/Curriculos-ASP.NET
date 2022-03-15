8e64539f4dbf67a4b66b10820a0e4615b2b55e1c
Nathan Bitu <nathan.bitu@gmail.com>
2022-03-15 10:35:44 -0300

    criando projeto e conexão com o banco
    Criando o projeto e adicionando a classe de conexão com o banco de dados

---
+++ Curriculos-ASP.NET/Curriculos-ASP.NET/DAOs/ConexaoBD.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace Curriculos_ASP.NET.DAOs
+{
+    public class ConexaoBD
+    {
+        public static SqlConnection GetConexao()
+        {
+            string cx = "data source=DESKTOP-2808IRF\\SQLEXPRESS; Database = Curriculos; integrated security = true";
+            SqlConnection conexao = new SqlConnection(cx);
+            conexao.Open();
+            return conexao;
+        }
+    }
+}
